38f7d842fd435fb23b61bf10b90e8d20509f29ab
Shivom Parashari <23bai70407@cuchd.in>
2026-02-19 23:19:15 +0530

    commit for exp

---
+++ exp-4/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: "20px", textAlign: "center" }}>
+      © 2025 Shivom Parashari
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ exp-4/src/components/ThemeToggle.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+function ThemeToggle() {
+  const { theme, toggleTheme } = useContext(AppContext);
+
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "light" ? "Dark Mode" : "Light Mode"}
+    </button>
+  );
+}
+
+export default ThemeToggle;
