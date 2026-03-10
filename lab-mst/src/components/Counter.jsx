53194d521b8c496913d0cb25904980ddd80205c0
Shivom Parashari <23bai70407@cuchd.in>
2026-03-10 14:37:20 +0530

    comit for lab mst

---
+++ lab-mst/src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Counter() {
+
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+
+      <button onClick={increment}>Increment</button>
+
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
